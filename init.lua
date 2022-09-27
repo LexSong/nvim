e42fd84531519ea791fc79c3f332dd92925afae4
Lex Song <LexSong.tw@gmail.com>
2022-09-27 18:28:30 +0800

    Config diagnostics virtual text

--- init.lua
+++ init.lua
@@ -43,6 +43,9 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.tabstop = 4
 
+-- Diagnostics
+vim.diagnostic.config({ virtual_text = { spacing = 1, prefix = "█" } })
+
 -- Set the python3 executable
 if vim.fn.has("win32") then
 	vim.api.nvim_set_var("python3_host_prog", "C:/Users/LexSong/mambaforge/python.exe")
