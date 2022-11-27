b5e55b70cd1128d6d39593d591e18f74da3fe09b
Lex Song <LexSong.tw@gmail.com>
2022-11-28 04:56:48 +0800

    Drop remapping for Colemak-DH
    Using cursor keys to move around instead

--- init.lua
+++ init.lua
@@ -5,25 +5,6 @@ vim.keymap.set("", "<C-W><Space>", ":")
 vim.keymap.set("", "<PageDown>", "<C-D>")
 vim.keymap.set("", "<PageUp>", "<C-U>")
 
--- Key mappings for Colemak-DH
-vim.keymap.set("", "m", "h")
-vim.keymap.set("", "n", "j")
-vim.keymap.set("", "e", "k")
-vim.keymap.set("", "i", "l")
-vim.keymap.set("", "M", "0")
-vim.keymap.set("", "N", "<C-D>")
-vim.keymap.set("", "E", "<C-U>")
-vim.keymap.set("", "I", "$")
-
-vim.keymap.set("", "h", "n")
-vim.keymap.set("", "j", "e")
-vim.keymap.set("", "k", "m")
-vim.keymap.set("", "l", "i")
-vim.keymap.set("", "H", "N")
-vim.keymap.set("", "J", "E")
-vim.keymap.set("", "K", "J")
-vim.keymap.set("", "L", "I")
-
 -- Map <C-E> to <C-P>
 vim.keymap.set("", "<C-E>", "<C-P>")
 vim.keymap.set("", "<C-P>", "<NOP>")
